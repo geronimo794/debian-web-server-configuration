2964d3c9058ce8b8d147dda7c2116a22a83822e2
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-04-27 15:51:14 +0700

    fix: Automation script

--- automated-script/install.sh
+++ automated-script/install.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
 
 # Load .env file
+set -o allexport
+source .env
+set +o allexport
 
-# Install nginx
+# Update all repository list
+sudo apt update
 
-# Install UFW
+# Install proxy-webserver
+source ./1.proxy-webserver.sh
 
-# Install Let's Encrypt
+# Install basic security
+source ./2.basic-security.sh
 
-# Install Fail2Ban
+# Install containerization application
+source ./3.containerization-application.sh
 
-# Install Docker
+# Install ci-cd pipeline
+source ./4.ci-cd-pipeline.sh
 
-# Install Jenkins on Docker
+# Install monitoring
+source ./5.monitoring.sh
 
-# Install Promotheus on Docker
-
-# Install Promotheus Node Exporter
-
-# Install Grafana on Docker
-
-# Define the new SSH port
-SSH_PORT=2020
-
-# Update SSH configuration
-sed -i 's/#Port 22/Port 2000/' /etc/ssh/sshd_config
-sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-
-# Restart the SSH service to apply the changes
-systemctl restart sshd
-
-# Output a message to confirm that the script has finished executing
-echo "SSH configuration has been updated. SSH port: $NEW_PORT, PermitRootLogin: no, PubkeyAuthentication: yes, PasswordAuthentication: no."
+# Done information
+echo "Installation Done, check and make sure every service is work!"
