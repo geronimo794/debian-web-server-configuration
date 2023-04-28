7c1c64d4852591f8c5f57ab4808eb241b49f1431
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-04-28 11:27:47 +0700

    feat: Container application

---
+++ automated-script/3.containerization-application.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install docker
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add repo to sourcelist
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Do repo update
+apt-get update
+
+# Install docker
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Test docker container
+docker run hello-world
