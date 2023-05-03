dc058e6639ec4e34182d81ba52edc36412c998f2
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-05-03 23:34:43 +0700

    fix: Manual configuration

--- automated-script/4.ci-cd-pipeline.sh
+++ automated-script/4.ci-cd-pipeline.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# Load .env file
+set -o allexport
+if [ ! -f .env ]; then
+	source format.env
+else
+	source .env
+fi
+set +o allexport
+
 # Create docker configuration for volume and network
 sudo docker network create jenkins
 sudo docker compose -f docker-compose-jenkins.yml up -d --build
 
 # Confirmation and setup on browser
 echo 
-echo "Open your terminal, and execute 'sudo docker logs jenkins-blueocean'"
-echo "Copy the password"
-echo "Access your Jenkins via browser at: http://..."
-echo "And paste your password ont the browser"
+echo "Manual configuration:"
+echo "1. Access your Jenkins via browser at: http://your.ip.addres:"$PORT_JENKINS
+echo "2. Wait until docker jenkins installation is done"
+echo "3. Open another terminal, and execute 'sudo docker logs jenkins-blueocean'"
+echo "4. Copy the password"
+echo "5. And paste your password ont the browser"
 read -p "Press enter to continue the process!"
