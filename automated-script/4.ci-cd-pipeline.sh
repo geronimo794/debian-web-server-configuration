bcfdf801e1be02366e4a1a4ac87864d717fd8c10
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-05-03 23:53:29 +0700

    fix: Get server IP Address

--- automated-script/4.ci-cd-pipeline.sh
+++ automated-script/4.ci-cd-pipeline.sh
@@ -13,10 +13,13 @@ set +o allexport
 sudo docker network create jenkins
 sudo docker compose -f docker-compose-jenkins.yml up -d --build
 
+# Get server ip address
+SERVER_IP_ADDRESS=$(curl ifconfig.me/ipifconfig.me/ip)
+
 # Confirmation and setup on browser
 echo 
 echo "Manual configuration:"
-echo "1. Access your Jenkins via browser at: http://your.ip.addres:"$PORT_JENKINS
+echo "1. Access your Jenkins via browser at: http://"$SERVER_IP_ADDRESS":"$PORT_JENKINS
 echo "2. Wait until docker jenkins installation is done"
 echo "3. Open another terminal, and execute 'sudo docker logs jenkins-blueocean'"
 echo "4. Copy the password"
